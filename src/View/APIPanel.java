c02ca03e8e19e9966ab613aa7571333640c98c18
Shimal <shimal.ibrahim@uni-oldenburg.de>
2013-05-27 20:16:49 +0200

    APIView hat jetzt auch ein Ergebnisfeld

--- src/View/APIPanel.java
+++ src/View/APIPanel.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import net.miginfocom.swing.MigLayout;
@@ -33,6 +34,8 @@ public class APIPanel {
 		panel.setOpaque(false);
 		// ------------------------------------------------
 		JPanel loginInfo = new JPanel(new MigLayout());
+		JPanel dbResultPanel = new JPanel(new MigLayout());
+		dbResultPanel.setBackground(Color.LIGHT_GRAY);
 		loginInfo.setBackground(Color.LIGHT_GRAY);
 		// loginInfo.setPreferredSize(new Dimension(loginInfo.getWidth(), 300));
 		// loginInfo.setMaximumSize(loginInfo.getPreferredSize());
@@ -60,6 +63,18 @@ public class APIPanel {
 		bg.add(rbWikiEN);
 
 		JButton loginBt = new JButton("Login");
+		
+		
+		//*****************
+		JLabel sqlResultlbl = new JLabel("Ergebnis: ");
+		JTextArea sqlResultField = new JTextArea();
+		sqlResultField.setPreferredSize(new Dimension(373,300));
+		sqlResultField.setLineWrap(true);
+		sqlResultField.setWrapStyleWord(true);
+		
+		JButton sendBt = new JButton("Senden");
+		//*****************
+		
 
 		loginInfo.add(wikiSitelbl, "gapright 30");
 		loginInfo.add(rbWikiDE);
@@ -72,8 +87,13 @@ public class APIPanel {
 		loginInfo.add(password);
 		loginInfo.add(passwordField);
 		loginInfo.add(loginBt);
+		
+		dbResultPanel.add(sqlResultlbl, "gapright 38");
+		dbResultPanel.add(sqlResultField, "span 3");
+		dbResultPanel.add(sendBt, "wrap");
 
-		panel.add(loginInfo, "wrap");
+		panel.add(loginInfo, "span");
+		panel.add(dbResultPanel, "span");
 
 	}
 
